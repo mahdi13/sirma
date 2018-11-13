adf106bc8069052c6c964701ee3c329a70d8903a
perfect <mahdi_1373@yahoo.com>
2018-11-13 14:27:54 +0300

    Adding models

--- src/Application.kt
+++ src/Application.kt
@@ -8,14 +8,35 @@ import io.ktor.http.*
 import io.ktor.sessions.*
 import io.ktor.auth.*
 import com.fasterxml.jackson.databind.*
+import io.ktor.config.ApplicationConfig
 import io.ktor.jackson.*
 import io.ktor.features.*
+import org.jetbrains.exposed.sql.Database
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-@Suppress("unused") // Referenced in application.conf
+
+fun initDatabase(config: ApplicationConfig) {
+    Database.connect(
+        "jdbc:postgresql://${config.property("host").toString()}:${config.property("port")}/${config.property("name").toString()}\n",
+        driver = "org.postgresql.Driver",
+        user = config.property("username").toString(),
+        password = config.property("password").toString()
+    )
+}
+
 @kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+fun Application.main(testing: Boolean = false) {
+
+
+    // This adds automatically Date and Server headers to each response, and would allow you to configure
+    // additional headers served to each response.
+    install(DefaultHeaders)
+
+    // Automatic '304 Not Modified' Responses
+    install(ConditionalHeaders)
+
+
     install(Sessions) {
         cookie<MySession>("MY_SESSION") {
             cookie.extensions["SameSite"] = "lax"
@@ -31,9 +52,14 @@ fun Application.module(testing: Boolean = false) {
         }
     }
 
+    initDatabase(environment.config.config("database"))
+
     routing {
         get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+            call.respondText(
+                environment.config.property("ktor.deployment.port")?.getString() ?: "an",
+                contentType = ContentType.Text.Plain
+            )
         }
 
         get("/session/increment") {
--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -16,10 +16,10 @@ import io.ktor.server.testing.*
 class ApplicationTest {
     @Test
     fun testRoot() {
-        withTestApplication({ module(testing = true) }) {
+        withTestApplication({ main(testing = true) }) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("HELLO WORLD!", response.content)
+                assertEquals("8080", response.content)
             }
         }
     }
